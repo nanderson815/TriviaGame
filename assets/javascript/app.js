d581b3a3caa9b172b1fddeb9ac5cb8659cd77319
Noah A <nanderson815@gmail.com>
2018-12-12 20:09:36 -0500

    Updated 12 12 18

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var questions = {
+        q1: {
+            qustion: "This is text question one?",
+            a1: "Choice 1",
+            a2: "Choie 2",
+            a3: "Choice 3",
+            correctAnswer: "Correct Choice",
+        }
+    }
+
+console.log(questions);
+
+});
+
