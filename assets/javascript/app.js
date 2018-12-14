0ea444a1fd479125e0752f041bc3461e814850ab
Noah A <nanderson815@gmail.com>
2018-12-13 22:18:34 -0500

    Updated 12 13 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
 
-    function Question(q, a1, a2, a3, a4) {
+    function Question(q, a1, a2, a3, a4, arrayNumber) {
         this.question = q
         this.answer1 = a1;
         this.answer2 = a2;
         this.answer3 = a3;
         this.answer4 = a4;
+        this.currectAnswerNumber = arrayNumber;
     }
 
-   var firstQuestion = new Question("Question one?", "answer 1", "answer 2", "correct answer", "answer 4" );
+    var firstQuestion = new Question("Question one?",
+        "answer 1", "answer 2", "correct answer", "answer 4", 2);
+        pushQuestions(firstQuestion);
 
+    function pushQuestions(questionNumber){
+        $("#question").html(questionNumber.question);
+        // console.log(firstQuestion.question);
+    }
 
     console.log(firstQuestion);
 
