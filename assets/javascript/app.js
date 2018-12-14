3bb56152d70776f20118bb2cb9e07777d046c81f
Noah A <nanderson815@gmail.com>
2018-12-13 22:35:18 -0500

    Updated 12 13 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,12 +11,20 @@ $(document).ready(function () {
 
     var firstQuestion = new Question("Question one?",
         "answer 1", "answer 2", "correct answer", "answer 4", 2);
-        pushQuestions(firstQuestion);
+    pushQuestions(firstQuestion);
 
-    function pushQuestions(questionNumber){
+    function pushQuestions(questionNumber) {
         $("#question").html(questionNumber.question);
-        // console.log(firstQuestion.question);
+        $("#a1").html(questionNumber.answer1);
+        $("#a2").html(questionNumber.answer2);
+        $("#a3").html(questionNumber.answer3);
+        $("#a4").html(questionNumber.answer4);
+
     }
+
+    $('#a1, #a2, #a3, #a4').on("click", function () {
+        console.log(this.value);
+    });
 
     console.log(firstQuestion);
 
