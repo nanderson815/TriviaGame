0d8a0e312f0384be8d3ec855c81b91793de22467
Noah A <nanderson815@gmail.com>
2018-12-15 10:47:08 -0500

    Updated 12 15 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    // consturctor for each question.
     function Question(q, a1, a2, a3, a4, arrayNumber) {
         this.question = q
         this.answer1 = a1;
@@ -10,7 +10,7 @@ $(document).ready(function () {
     }
 
 
-
+    // variables for each question that are passed into the constructor
     var question0 = new Question("Who played Noah in the movie 'The Notebook'?",
         "Colin Ferrell", "Danny Devito", "Ryan Gossling", "Brad Pitt", 2);
 
@@ -41,10 +41,12 @@ $(document).ready(function () {
     var question9 = new Question("Baby, from the 2017 film 'Baby Driver', has what condition?",
         "Schizophrenia", "Tinnitus", "Aids", "Night Terrors", 1);
 
+    // Each question is stored in this array so that we can use the index number to pass questions to the DOM. I need to write a function that automatically passes questions into the arry.
     var questionList = [
         question0, question1, question2, question3, question4, question5, question6, question7, question8, question9
     ];
 
+    // Function to push question to the DOM.
     function pushQuestions(questionNumber) {
         $("#question").html(questionNumber.question);
         $("#a1").html(questionNumber.answer1);
@@ -53,24 +55,29 @@ $(document).ready(function () {
         $("#a4").html(questionNumber.answer4);
 
     }
-
+    // Vars that store the number of wins, losses, i for current question, and a global var to hold what element was clicked
     var wins = 0;
     var losses = 0;
     var i = 0;
     var clickedValue;
 
+    // Timer variable
     var timer = 30;
 
+    // Function to reset the value of the clock and push to DOM.
     function resetTimer() {
         timer = 30;
         $("#timeRemaining").html(timer);
     }
 
-
+    // Push the first question on load.
     pushQuestions(questionList[i]);
 
+    // interval to start the countdown timer on load
     setInterval(count, 1000);
 
+    // Function that counts down by 1 if timer is above zero, or sends the next question if time expires. 
+    // ClickedValue to null ensures that time expired is counted as a loss.
     function count() {
         if (timer > 0) {
             timer--;
