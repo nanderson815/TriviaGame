f422ce01437c368aed94ddf13dac04123f518275
Noah A <nanderson815@gmail.com>
2018-12-16 20:28:30 -0500

    Updated 12 16 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,46 @@
 $(document).ready(function () {
     // consturctor for each question.
-    function Question(q, a1, a2, a3, a4, arrayNumber) {
+    function Question(q, a1, a2, a3, a4, arrayNumber, answer) {
         this.question = q
         this.answer1 = a1;
         this.answer2 = a2;
         this.answer3 = a3;
         this.answer4 = a4;
         this.currectAnswerNumber = arrayNumber;
+        this.correctAnswer = answer;
     }
 
 
     // variables for each question that are passed into the constructor
     var question0 = new Question("Who played Noah in the movie 'The Notebook'?",
-        "Colin Ferrell", "Danny Devito", "Ryan Gossling", "Brad Pitt", 2);
+        "Colin Ferrell", "Danny Devito", "Ryan Gossling", "Brad Pitt", 2, "Ryan Gossling");
 
     var question1 = new Question("What is Brennan's special talent in the movie 'Step Brothers'?",
-        "Dancing", "Kissing", "Dog Training", "Singing", 3);
+        "Dancing", "Kissing", "Dog Training", "Singing", 3, "Singing");
 
     var question2 = new Question("In what city does the crime thriller 'Black Mass' about the life of the infamous Whitey Bulger take place?",
-        "Chicago", "Boston", "New York", "Los Angeles", 1);
+        "Chicago", "Boston", "New York", "Los Angeles", 1, "Boston");
 
     var question3 = new Question("In the 2009 film 'Inglourious Basterds', Hans Landa has a reputation for hunting what?",
-        "Jews", "Wolfs", "Nazis", "Fugitives", 0);
+        "Jews", "Wolfs", "Nazis", "Fugitives", 0, "Jews");
 
     var question4 = new Question("Wall Street guru Michael Burry makes a fortune purchasing what security in the movie 'The Big Short'?",
-        "AAA Rated Corporate Bonds", "Penny Stocks", "Mortgage Backed Securities", "Credit Default Swaps", 3);
+        "AAA Rated Corporate Bonds", "Penny Stocks", "Mortgage Backed Securities", "Credit Default Swaps", 3 ,"Credit Default Swaps");
 
     var question5 = new Question("The Wolf of Wall Street, AKA Jordan Belfort, is best categorized as a what?",
-        "Genius", "Fraudster", "Family Man", "Innovator", 1);
+        "Genius", "Fraudster", "Family Man", "Innovator", 1, "Frauster");
 
     var question6 = new Question("In the film 'Django Unchained', Dr. King Schultz is in what profession?",
-        "Optometry", "Dentistry", "Law", "Finance", 1);
+        "Optometry", "Dentistry", "Law", "Finance", 1, "Dentistry");
 
     var question7 = new Question("Andrew, the main character in the movie Whiplash, plays what instrument?",
-        "Piano", "Chello", "Bass Guitar", "Drums", 3);
+        "Piano", "Chello", "Bass Guitar", "Drums", 3, "Drums");
 
     var question8 = new Question("In The Life of Pi, Pi shares his life raft with what?",
-        "A Tiger", "A Beaver", "A Dog", "A Racoon", 0);
+        "A Tiger", "A Beaver", "A Dog", "A Racoon", 0, "A Tiger");
 
     var question9 = new Question("Baby, from the 2017 film 'Baby Driver', has what condition?",
-        "Schizophrenia", "Tinnitus", "Aids", "Night Terrors", 1);
+        "Schizophrenia", "Tinnitus", "Aids", "Night Terrors", 1, "Tinnitus");
 
     // Each question is stored in this array so that we can use the index number to pass questions to the DOM. I need to write a function that automatically passes questions into the arry.
     var questionList = [
@@ -130,7 +131,7 @@ $(document).ready(function () {
             i++;
             displayModal("Thats correct!");
             setTimeout(hideModal, 3000);
-            setTimeout(resetTimer, 3000);
+            resetTimer();
             if (i < 10) {
                 pushQuestions(questionList[i]);
             } else {
@@ -140,9 +141,8 @@ $(document).ready(function () {
         } else {
             losses++;
             $(".losses").text(losses);
+            displayModal("Wrong! The correct answer is: " + questionList[i].correctAnswer);
             i++;
-            displayModal("Wrong! The correct answer is: " + questionList[i]);
-            console.log(questionList[i]);
             setTimeout(hideModal, 3000);
             resetTimer();
             if (i < 10) {
